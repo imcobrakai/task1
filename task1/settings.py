6e74e0aed2be2cebc38c0c11627cdaa0aec3b765
kai <kailashjat604@gmail.com>
2022-08-19 12:54:08 +0530

    Update settings.py

--- task1/settings.py
+++ task1/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = (
-    BASE_DIR/ "static",
+    BASE_DIR/ "staticfiles",
 )
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
